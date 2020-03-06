3065e9179eeb5e84b876937c0b156b4fb0d598c0
root <root@localhost.localdomain>
2020-03-06 17:24:06 +0100

    fixed database env

--- ChallengeDashboard/routes/web.php
+++ ChallengeDashboard/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return "login";
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
